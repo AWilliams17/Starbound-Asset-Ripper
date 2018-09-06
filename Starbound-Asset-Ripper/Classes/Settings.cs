bfc0b4e3d8b53d7df058a6025c8349dbe2178ab0
awilliams17 <awilliams17411@gmail.com>
2018-09-06 18:16:32 -0500

    I really need to make a proper config handler for c#...

---
+++ Starbound-Asset-Ripper/Classes/Settings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace Starbound_Asset_Ripper
+{
+    public class Settings
+    {
+        private string _steamDirectory = "";
+        private string _outputDirectory = "";
+
+        public string SteamDirectory
+        {
+            get => _steamDirectory;
+            set => _steamDirectory = value;
+        }
+
+        public string OutputDirectory
+        {
+            get => _outputDirectory;
+            set => _outputDirectory = value;
+        }
+
+        public void SaveSettings()
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\Software\\StarboundAssetRipper", "SteamDirectory", SteamDirectory, RegistryValueKind.String);
+            Registry.SetValue("HKEY_CURRENT_USER\\Software\\StarboundAssetRipper", "OutputDirectory", OutputDirectory, RegistryValueKind.String);
+        }
+
+        public void LoadSettings() // This is bad, but it works.
+        {
+            RegistryKey StarboundAssetRipperRegistry = Registry.CurrentUser.OpenSubKey("Software\\StarboundAssetRipper", false);
+            if (StarboundAssetRipperRegistry == null)
+            {
+                SaveSettings();
+            }
+            else
+            {
+                SteamDirectory = StarboundAssetRipperRegistry.GetValue("SteamDirectory").ToString();
+                OutputDirectory = StarboundAssetRipperRegistry.GetValue("OutputDirectory").ToString();
+            }
+        }
+    }
+    class Validators
+    {
+
+    }
+}
