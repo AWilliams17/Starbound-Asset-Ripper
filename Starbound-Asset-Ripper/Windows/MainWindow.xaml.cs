0740fbe92d11d46b7710b9fd311a42ae7d418313
awilliams17 <awilliams17411@gmail.com>
2018-09-09 23:40:58 -0500

    added refresh pak list button, planning the LoadPak() function

--- Starbound-Asset-Ripper/Windows/MainWindow.xaml.cs
+++ Starbound-Asset-Ripper/Windows/MainWindow.xaml.cs
@@ -3,8 +3,9 @@ using System.Windows;
 using System.Windows.Media;
 using Starbound_Asset_Ripper.ConfigContainer;
 using ApplicationUtils;
-using Microsoft.Win32;
+using System.Collections.Generic;
 
+// TODO: Need to add threading around the web utils function calls
 namespace Starbound_Asset_Ripper
 {
     /// <summary>
@@ -15,6 +16,7 @@ namespace Starbound_Asset_Ripper
         private bool steamPathSet = false;
         private bool outputPathSet = false;
         private bool workshopPathSet = false;
+        private Dictionary<string, string> pakDictionary = new Dictionary<string, string>();
         public static Config config = new Config();
 
         public MainWindow()
@@ -35,7 +37,7 @@ namespace Starbound_Asset_Ripper
             workshopPathSet = (config.settings.GetOption<string>("WorkshopPath") != "");
             outputPathSet = (config.settings.GetOption<string>("OutputPath") != "");
 
-            UpdateControls();
+            UpdateAllControls();
         }
 
         private void OutputPathBtn_Click(object sender, RoutedEventArgs e)
@@ -46,7 +48,7 @@ namespace Starbound_Asset_Ripper
                 config.settings.SetOption("OutputPath", outputPath);
                 config.settings.SaveSettings();
                 outputPathSet = true;
-                UpdateControls();
+                UpdateAllControls();
             }
         }
 
@@ -72,7 +74,7 @@ namespace Starbound_Asset_Ripper
                 config.settings.SetOption("SteamPath", steamPath);
                 config.settings.SaveSettings();
                 steamPathSet = true;
-                UpdateControls();
+                UpdateAllControls();
             }
         }
 
@@ -100,10 +102,20 @@ namespace Starbound_Asset_Ripper
         {
             WebUtilsRelated.HandleUpdateCheck();
         }
-
+        
         private void LoadPaks()
         {
-
+            // Get workshop directory path
+            // Get starbound asset unpacker path (and if it doesn't exist scream about it)
+            // Now go into the workshop directory, and
+            /*
+             * For each folder in there,
+             *  If the folder has a .pak file, create a string ".pak in folder: {folder name}", take the .pak file path,
+             *      Now store the ".pak in folder" string as a key in the pakList dictionary, with the value being the pak file path.
+             *  Otherwise, keep going through all the folders.
+             * Now, after this is done, 
+             *  
+            */
         }
 
         private void UnpackSelectedBtn_Click(object sender, RoutedEventArgs e)
@@ -116,40 +128,46 @@ namespace Starbound_Asset_Ripper
 
         }
 
+        private void RefreshPakListBtn_Click(Object sender, RoutedEventArgs e)
+        {
+            pakDictionary.Clear();
+            LoadPaks();
+        }
+
         private void MainWindow_Closing(object sender, EventArgs e)
         {
             config.settings.SaveSettings();
         }
 
-        private void SetLabels()
+        private void SetStatusLabel(string LabelText, Brush StatusColor)
         {
-            void SetLabelContent(string LabelText, Brush StatusColor)
-            {
-                StatusLabel.Content = LabelText;
-                StatusLabel.Foreground = StatusColor;
-            }
+            StatusLabel.Content = LabelText;
+            StatusLabel.Foreground = StatusColor;
+        }
 
+        private void UpdateLabels()
+        {
             if (steamPathSet && outputPathSet)
             {
                 if (!workshopPathSet)
                 {
-                    SetLabelContent("Starbound workshop folder not found in Steam folder.", LabelColors.Bad);
+                    SetStatusLabel("Starbound workshop folder not found in Steam folder.", LabelColors.Bad);
                 }
                 else
                 {
                     UnpackAllBtn.IsEnabled = true;
                     UnpackSelectedBtn.IsEnabled = true;
-                    SetLabelContent("Everything looks good.", LabelColors.Good);
+                    SetStatusLabel("Everything looks good.", LabelColors.Good);
                 }
             }
             else
             {
-                SetLabelContent("Waiting for paths to be set...", LabelColors.Bad);
+                SetStatusLabel("Waiting for paths to be set...", LabelColors.Bad);
             }
 
         }
 
-        private void SetTextBoxes()
+        private void UpdateTextBoxes()
         {
             SteamPathTextBox.Text = "Path to Steam installation...";
             OutputPathTextBox.Text = "Path to output folder...";
@@ -164,10 +182,10 @@ namespace Starbound_Asset_Ripper
             }
         }
 
-        private void UpdateControls()
+        private void UpdateAllControls()
         {
-            SetTextBoxes();
-            SetLabels();
+            UpdateTextBoxes();
+            UpdateLabels();
             if (steamPathSet && workshopPathSet && outputPathSet)
             {
                 LoadPaks();
